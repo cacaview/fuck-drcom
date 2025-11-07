a424a96a9a95d96c8d44f9e45df86afd9cd82235
cacaview <1475429618@qq.com>
2025-11-07 23:08:18 +0800

    修复bug和优化代码

--- common/config_manager.py
+++ common/config_manager.py
@@ -16,8 +16,7 @@ import json
 import getpass
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2
-from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
 
 
@@ -50,12 +49,11 @@ class ConfigManager:
         Returns:
             bytes: 派生的密钥
         """
-        kdf = PBKDF2(
+        kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
             length=32,
             salt=salt,
-            iterations=100000,
-            backend=default_backend()
+            iterations=100000
         )
         return base64.urlsafe_b64encode(kdf.derive(password.encode()))
     
